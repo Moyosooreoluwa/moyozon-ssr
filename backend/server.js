2cc1dc5731261defe2981469ad2d240c09522444
Moyosoore Ayo-Idowu <moyosooreai@gmail.com>
2025-03-26 02:16:17 +0100

    Implement Signin API

--- backend/server.js
+++ backend/server.js
@@ -5,11 +5,14 @@ import data from './data.js';
 import mongoose from 'mongoose';
 import seedRouter from './routes/seedRoutes.js';
 import productRouter from './routes/productRoutes.js';
+import userRouter from './routes/userRoutes.js';
 
 // Load environment variables
 dotenv.config();
 
 const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
 mongoose
   .connect(process.env.MONGODB_URI)
@@ -31,6 +34,12 @@ app.use(express.json()); // For parsing application/json
 
 app.use('/api/seed', seedRouter);
 app.use('/api/products', productRouter);
+app.use('/api/users', userRouter);
+
+// error handler
+app.use((err, req, res, next) => {
+  res.status(500).send({ message: err.message });
+});
 
 // Start the server
 const PORT = process.env.PORT || 5000;
